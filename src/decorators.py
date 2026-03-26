77b331a9c73dd8644490ad7cbeb4533d72654332
Липатов Михаил <mikl_slony@inbox.ru>
2026-03-26 18:51:43 +0300

    результат работы линтера black фактических изменений нет

--- src/decorators.py
+++ src/decorators.py
@@ -5,13 +5,14 @@ from typing import Any, Callable, Mapping, Optional, Tuple
 
 def log(filename: Optional[str] = None) -> Callable:
     """Декоратор для логирования"""
+
     def log_msg(c_msg: str) -> None:
         if filename:
-            my_file = open(os.path.join(os.curdir, filename), 'a', encoding='cp1251')
+            my_file = open(os.path.join(os.curdir, filename), "a", encoding="cp1251")
             if my_file.__sizeof__() == 0:
                 my_file.write(c_msg)
             else:
-                my_file.write('\n' + c_msg)
+                my_file.write("\n" + c_msg)
             my_file.close()
         else:
             print(c_msg)
@@ -21,10 +22,10 @@ def log(filename: Optional[str] = None) -> Callable:
         def wrapper(*args: Tuple, **kwargs: Mapping) -> Any:
             try:
                 result = func(*args, **kwargs)
-                log_msg(func.__name__+' Ок')
+                log_msg(func.__name__ + " Ок")
                 return result
             except Exception as e:
-                log_msg(f'{func.__name__} error:{str(e)} . Inputs:{args} {kwargs}')
+                log_msg(f"{func.__name__} error:{str(e)} . Inputs:{args} {kwargs}")
                 return None
 
         return wrapper
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -6,22 +6,22 @@ from src.decorators import log
 def test_log(capsys: pytest.CaptureFixture[str]) -> None:
     @log()
     def some_text() -> str:
-        return 'Какой то текст'
+        return "Какой то текст"
 
     @log()
     def some_err() -> float:
-        return 1000/0
+        return 1000 / 0
 
     @log()
     def some_err2(i_in: int, i_in2: int) -> float:
-        return i_in/0
+        return i_in / 0
 
     some_text()
     captured = capsys.readouterr()
-    assert captured.out == 'some_text Ок\n'
+    assert captured.out == "some_text Ок\n"
     some_err()
     captured = capsys.readouterr()
-    assert captured.out == 'some_err error:division by zero . Inputs:() {}\n'
+    assert captured.out == "some_err error:division by zero . Inputs:() {}\n"
     some_err2(1001, 1)
     captured = capsys.readouterr()
-    assert captured.out == 'some_err2 error:division by zero . Inputs:(1001, 1) {}\n'
+    assert captured.out == "some_err2 error:division by zero . Inputs:(1001, 1) {}\n"
