c79f3ff2d4638710e826f68a9be1d74f5030db5d
Липатов Михаил <mikl_slony@inbox.ru>
2026-03-26 19:08:32 +0300

    utils   get_json_attr_date, get_json_attr_float, get_json_attr_str  - функции получения  значений из JSON c помощью JSONpath   read_json_trn - чтение файла с проводками external_api   fTrnAmount  - сумма проводки   get_rate    - пересчет суммы проводки в рублевый эквивалент курсу на дату проводки test_... - тесты для новых функций conftest - добавлен test_json_trn - для тестов новых функций data - файлы json для работы/проверки новых функций

---
+++ src/utils.py
@@ -0,0 +1,56 @@
+import json
+import os
+from datetime import datetime
+
+from jsonpath_ng import parse  # type: ignore
+
+
+def get_json_attr_float(data: dict, path: str) -> float | None:
+    """функция получения значения атрибута из json по пути атрибута для float"""
+    jsonpath_expression = parse(path)
+    matches = jsonpath_expression.find(data)
+    if matches == []:
+        print(f"{path} not found")
+        return None
+    else:
+        return float(matches[0].value)
+
+
+def get_json_attr_str(data: dict, path: str) -> str | None:
+    """функция получения значения атрибута из json по пути атрибута для строки"""
+    jsonpath_expression = parse(path)
+    matches = jsonpath_expression.find(data)
+    if matches == []:
+        print(f"{path} not found")
+        return None
+    else:
+        return str(matches[0].value)
+
+
+def get_json_attr_date(data: dict, path: str) -> datetime | None:
+    """функция получения значения атрибута из json по пути атрибута для даты"""
+    jsonpath_expression = parse(path)
+    matches = jsonpath_expression.find(data)
+    if matches == []:
+        print(f"{path} not found")
+        return None
+    else:
+        return datetime.strptime(str(matches[0].value), "%Y-%m-%dT%H:%M:%S.%f")  # 2018-11-23T17:47:33.127140
+
+
+def read_json_trn(cFileName: str, cEncoding: str = "UTF-8") -> list:
+    """Функция чтения JSON из файла"""
+    if os.path.exists(cFileName) == 0:
+        return list()
+    elif os.path.getsize(cFileName) == 0:
+        return list()
+    else:
+        with open(cFileName, encoding=cEncoding) as f:
+            data = list(json.load(f))
+            f.close()
+        if data is None:
+            return list()
+        elif isinstance(data, list) == 0:
+            return list()
+        else:
+            return data
