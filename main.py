6ecde21ec56173de46ca66a7d18ce219f54e2258
Липатов Михаил <mikl_slony@inbox.ru>
2026-05-03 18:42:11 +0300

    Домашка 13_2

--- main.py
+++ main.py
@@ -1,14 +1,83 @@
-from src.log_mng import mainLog
-from src.masks import get_mask_account
-# from src.processing import filter_by_state, sort_by_date
-# from src.widget import get_date, mask_account_card
-from src.utils import get_json_attr_float, get_json_attr_str, read_json_trn
+from src.csv_xls_utils import json2flat, read_csv_trn, read_xls_trn
+from src.input_utils import get_input, oper_msg
+from src.processing import filter_by_cur, filter_by_dscr, filter_by_state, sort_by_amount, sort_by_date
+from src.utils import read_json_trn
+from src.widget import mask_account_card
 
-lst_trn = read_json_trn("c://PYTHON_PRJ//bnk_app//data//empty.json")
-lst_trn = read_json_trn("c://PYTHON_PRJ//bnk_app//data//nonexist.json")
-lst_trn = read_json_trn("c://PYTHON_PRJ//bnk_app//data//operations.json")
-mainLog().info(f"get_mask_account('123') {get_mask_account(123)}")
-mainLog().info(
-    f"Сумма какой попало проводки {get_json_attr_float(lst_trn[0], "operationAmount.amount")} "
-    + f"{get_json_attr_str(lst_trn[0], "operationAmount.currency.code")}"
-)
+FILE_TYPES = {"1": "JSON", "2": "CSV", "3": "XLSX"}
+
+
+def main() -> None:
+    # ---------------------------------------------------------------------------
+    str_1 = get_input(
+        "Привет! Добро пожаловать в программу работы \n"
+        "с банковскими транзакциями. \n"
+        "Выберите необходимый пункт меню:)\n"
+        "    1. Получить информацию о транзакциях из JSON-файла\n"
+        "    2. Получить информацию о транзакциях из CSV-файла\n"
+        "    3. Получить информацию о транзакциях из XLSX-файла\n"
+        "    ",
+        list(FILE_TYPES.keys()),
+    )
+    print(f"Для обработки выбран {FILE_TYPES[str_1]}- файл.")
+    if str_1 == "1":
+        lst_trn = json2flat(read_json_trn("c://PYTHON_PRJ//bnk_app//data//operations.json"))
+    elif str_1 == "2":
+        lst_trn = read_csv_trn("c://PYTHON_PRJ//bnk_app//data//transactions.csv")
+    else:
+        lst_trn = read_xls_trn("c://PYTHON_PRJ//bnk_app//data//transactions_excel.xlsx")
+    print(lst_trn[0])
+    # ---------------------------------------------------------------------------
+    str_1 = get_input(
+        "Программа: Введите статус, по которому необходимо выполнить фильтрацию.\n"
+        + "Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING ",
+        ["EXECUTED", "CANCELED", "PENDING"],
+        fMsg=oper_msg,
+        cUp=True,
+    )
+    lst_trn = filter_by_state(lst_trn, str_1)
+    print(f"Операции отфильтрованы по статусу {str_1}")
+    # ---------------------------------------------------------------------------
+    str_1 = get_input("Отсортировать операции по дате? Да / Нет", ["Да", "Нет"], cUp=True)
+    if str_1 == "ДА":
+        lst_trn = sort_by_date(lst_trn)
+        print("Операции отсортированы по дате")
+    # ---------------------------------------------------------------------------
+    str_1 = get_input(
+        "Отсортировать по возрастанию или по убыванию?",
+        ["возрастанию", "убыванию"],  # саздизм в чистом виде - почему не выбирать цифры везде?
+        cUp=True,
+    )
+    if str_1 == "ВОЗРАСТАНИЮ":
+        lst_trn = sort_by_amount(lst_trn)
+        print("Операции отсортированы по возрастанию")
+    else:
+        lst_trn = sort_by_amount(lst_trn, True)
+        print("Операции отсортированы по убыванию")
+    # ---------------------------------------------------------------------------
+    str_1 = get_input("Выводить только рублевые транзакции? Да/Нет", ["Да", "Нет"], cUp=True)
+    if str_1 == "ДА":
+        lst_trn = filter_by_cur(lst_trn, "RUB")
+        print("Операции отфильтрованы по валюте = 'RUB'")
+    # ---------------------------------------------------------------------------
+    str_1 = get_input(
+        "Отфильтровать список транзакций по определенному слову в описании? Да/Нет", ["Да", "Нет"], cUp=True
+    )
+    if str_1 == "ДА":
+        str_1 = ""
+        while len(str_1) == 0:
+            str_1 = str(input("Введите слово"))
+        lst_trn = filter_by_dscr(lst_trn, str_1)
+        print(f"Операции отфильтрованы по слову = '{str_1}'")
+    # ---------------------------------------------------------------------------
+    if len(lst_trn) == 0:
+        print("Не найдено ни одной транзакции, подходящей под ваши условия фильтрации")
+    else:
+        print(f"Всего банковских операций в выборке: {len(lst_trn)}")
+    for trn in lst_trn:
+        print(f"{trn["date"]} {trn["description"]}")
+        print(f"{mask_account_card(trn["from"])} --> {mask_account_card(trn["to"])}")
+        print(f"Сумма:{mask_account_card(trn["from"])} --> {mask_account_card(trn["to"])}")
+
+
+main()
--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,4 @@
+import re
 from typing import List
 
 from src.widget import get_date_real
@@ -14,3 +15,19 @@ def sort_by_date(in_dict_list: List[dict], in_reverse: bool = True) -> List[dict
     """Функция должна возвращать новый список, отсортированный по дате (date)"""
     ret_dict = sorted(in_dict_list, key=lambda one_rec: get_date_real(one_rec["date"]), reverse=in_reverse)
     return ret_dict
+
+
+def sort_by_amount(in_dict_list: List[dict], in_reverse: bool = True) -> List[dict]:
+    """Функция должна возвращать новый список, отсортированный по дате (date)"""
+    ret_dict = sorted(in_dict_list, key=lambda one_rec: float(one_rec["amount"]), reverse=in_reverse)
+    return ret_dict
+
+
+def filter_by_cur(in_dict_list: list[dict], search: str) -> list[dict]:
+    ret_dict = [one_dict for one_dict in in_dict_list if one_dict["currency_code"] == search]
+    return ret_dict
+
+
+def filter_by_dscr(in_dict_list: list[dict], search: str) -> list[dict]:
+    pattern = re.compile(search)
+    return [one_dict for one_dict in in_dict_list if pattern.search(str(one_dict.get("description"))) is not None]
